986c6d12a53e007e14275f573016a9d863c4eee1
Readush Shalihin <67329789+rsreyaad143@users.noreply.github.com>
2023-01-29 20:34:48 +0600

    Add files via upload

---
+++ prectice/js/main.js
@@ -0,0 +1,10 @@
+ // bx slider
+
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_manu').meanmenu({
+      meanMenuContainer: '#mobile_menu',
+      meanScreenWidth: 991,
+    }
+    );
+  });
